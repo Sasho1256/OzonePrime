32ed647b5424c3051533202b2d8db6b08559bd5b
Valeri Tonchev <v.tonchev2003@gmail.com>
2021-03-13 20:48:37 +0200

    starting cast

---
+++ Services/CastService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OzonePrime.Models;
+
+namespace OzonePrime.Services
+{
+    public class CastService
+    {
+        private ozoneprimeContext database;
+
+        public CastService(ozoneprimeContext database)
+        {
+            this.database = database;
+        }
+    }
+}
