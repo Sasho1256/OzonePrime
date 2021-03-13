057b85559ad533b43ac0506a44ee27b4d04e8cab
Valeri Tonchev <v.tonchev2003@gmail.com>
2021-03-13 20:39:48 +0200

    little changes

--- Services/FilmService.cs
+++ Services/FilmService.cs
@@ -30,24 +30,5 @@ namespace OzonePrime.Services
             database.Films.Add(film);
             database.SaveChanges();
         }
-
-        /*
-        public void AddFilm(Film film)
-        {
-            database.Films.Add(film);
-        }
-
-        public void EditFilm(int id, Film film)
-        {
-            Film filmToEdit = database.Films.FirstOrDefault(x => x.Id == id);
-            filmToEdit = film;
-        }
-
-        public void RemoveFilm(int id)
-        {
-            Film filmToRemove = database.Films.FirstOrDefault(x => x.Id == id);
-            database.Films.Remove(filmToRemove);
-        }
-        */
     }
 }
